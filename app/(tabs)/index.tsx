b0880e3e0faa073d8e532f2d3b0e01baa03f944c
mirjamkokkonen2-cmd <mirjamkokkonen2@gmail.com>
2026-01-06 23:48:32 +0200

    muokkaauksiiaa

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,7 +17,9 @@ export default function HomeScreen() {
            <Viikko weekday={5}/>
            <Viikko weekday={6}/>
     </View>
-        <View style={{backgroundColor: '#908afbff', width: '100%'}}><p>Icon made by Freepik from www.flaticon.com</p></View>
+        <View style={{height: '10%', width: '100%'}}><p></p></View>
+        <View style={{backgroundColor: '#908afbff', height: '30%', width: '100%'}}><p>  Credits:
+            Icon made by Freepik from www.flaticon.com</p></View>
         </ScrollView>
     </View>
   );
